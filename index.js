5fd7e497a2435261afbd52311660aadd4fa809d7
Bludupdup <74095650+Bludupdup@users.noreply.github.com>
2026-07-06 03:15:37 +0530

    Add files via upload
    improved a lot of css added day and night feature made the toggle button work properly .. one last feature left fetch current location data

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
 const tenDayForecast = document.getElementById("tenDayForecast");
 
-const days = [
-    "Today",
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
-    "Sun",
-    "Mon"
-];
-
-for (let i = 0; i < 7; i++) {
-    const card = document.createElement("div");
-    card.classList.add("dayCard");
-
-    card.innerHTML = `
-        <h1>${days[i]}</h1>
-
-        <div class="preciCard">
-            <h1>☁️</h1>
-            <h1>60%</h1>
-        </div>
-
-        <h1>${27 + i % 3}°</h1>
-    `;
-
-    tenDayForecast.appendChild(card);
-}
-
-//
-
 
 const apiKey = "be96fa9bd95b7ba44b77b0087a668a30";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
@@ -54,7 +21,7 @@ async function getWeather(city){
 
 
     if(response.status == 404){
-        alert("invalid inoput");
+        alert("invalid input");
         return;
     }else{
 
@@ -70,7 +37,7 @@ async function getWeather(city){
         document.querySelector("#low").innerHTML = "L: " + data.main.temp_min + "°";
 
         document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed ;
+        document.querySelector(".wind").innerHTML = data.wind.speed + " kph" ;
 
         document.querySelector("#feelsLikeTemp").innerHTML = data.main.feels_like + "°";
         document.querySelector("#pressure").innerHTML = data.main.pressure + " hPa";
@@ -94,6 +61,34 @@ async function getWeather(city){
 
         document.querySelector("#visibility").innerHTML = `${(data.visibility / 1000).toFixed(1)} km`;
 
+        //change bg
+        const currentTime = data.dt;
+        const sunrise = data.sys.sunrise;
+        const sunset = data.sys.sunset;
+
+        const isDay = currentTime >= sunrise && currentTime < sunset
+
+        let weatherType;
+
+        switch(data.weather[0].main){
+
+            case "Clear":
+                weatherType = "clear";
+                break;
+
+            case "Clouds":
+                weatherType = "cloudy";
+                break;
+
+            default:
+                weatherType = "rain";
+        }
+
+        const time = isDay ? "day" : "night";
+
+        const videoFile =`./${weatherType}_${time}.mp4`;
+
+
         dayForecast.innerHTML = " ";
         for(let i = 0; i < 8; i++){
             const hour = forecastData.list[i];
@@ -102,12 +97,21 @@ async function getWeather(city){
             const card = document.createElement("div");
             card.classList.add("hourCard");
 
-        card.innerHTML = `
-                            <h1>${time}</h1>
-                            <h1>${hour.weather[0].main}</h1>
-                            <h1>${Math.round(hour.main.temp)}°</h1>`;
+            const icon = hour.weather[0].icon;
+            const iconUrl =`https://openweathermap.org/img/wn/${icon}@2x.png`;
 
-        dayForecast.appendChild(card);
+            const icon1 = data.weather[0].icon;
+            const iconUrl1 = `https://openweathermap.org/img/wn/${icon1}@4x.png`;
+
+            const weatherImage = document.querySelector("#weatherImg");
+            weatherImage.src = iconUrl1;
+
+            card.innerHTML = `
+                                <h1>${time}</h1>
+                                <img src="${iconUrl}" class="hourWeatherIcon">
+                                <h1>${Math.round(hour.main.temp)}°</h1>`;
+
+            dayForecast.appendChild(card);
 
         }
 
@@ -120,31 +124,57 @@ async function getWeather(city){
             const date = new Date(day.dt * 1000);
 
             const dayName = date.toLocaleDateString("en-US",{
-                weekday: "long"
+                weekday: "short"
             });
 
             const card = document.createElement("div");
             card.classList.add("dayCard");
 
-            card.innerHTML = `<h1>${dayName}</h1>
-                            <div class="preciCard">
-                                <h1>${day.weather[0].main}</h1>
-                                <h1>${Math.round(day.pop * 100)}%</h1>
-                            </div>
-                            <div>
-                            <h1>H: ${Math.round(day.main.temp_max)}°</h1>
-                            <h1>L: ${Math.round(day.main.temp_min)}°</h1>
+            const dayicon = day.weather[0].icon;
+            const dayiconUrl = `https://openweathermap.org/img/wn/${dayicon}@2x.png`;
 
-                            </div>`;
+            card.innerHTML = `  <div class="forecastLeft">
+                                    <h3>${dayName}</h3>
+                                </div>
+
+                                <div class="forecastCenter">
+                                    <img src="${dayiconUrl}" class="dayWeatherIcon">
+                                    <span>${Math.round(day.pop * 100)}%</span>
+                                </div>
+
+                                <div class="forecastRight">
+                                    <h3>H ${Math.round(day.main.temp_max)}°</h3>
+                                    <h3>L ${Math.round(day.main.temp_min)}°</h3>
+                                </div>`;
             tenDayForecast.appendChild(card);
 
         }
 
+        if(!bgVideo.src.includes(videoFile))
+        {
+            bgVideo.style.opacity = 0;
+
+            setTimeout(() => {
+
+                bgVideo.src = videoFile;
+                bgVideo.load();
+
+                bgVideo.onloadeddata = () => {
+                    bgVideo.play();
+                    bgVideo.playbackRate = 0.8;
+                    bgVideo.style.opacity = 1;
+                };
+
+            }, 300);
+        }
+
+
+
     }
-   
+   return data;
 }
 
-searchButton.addEventListener("click", ()=>{
+searchButton.addEventListener("click",async ()=>{
 
     const city = input.value;
 
@@ -160,7 +190,20 @@ searchButton.addEventListener("click", ()=>{
 
         const card = document.createElement("div");
         card.classList.add("newCityCard");
-        card.textContent = city;
+        
+        const weather = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        const cityData = await weather.json();
+
+        const icon = cityData.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
+
+        card.innerHTML =`<div class="cityInfo">
+                            <h3>${cityData.name}</h3>
+                        </div>
+
+                        <img src="${iconUrl}" class="savedCityIcon">
+
+                        <h2>${Math.round(cityData.main.temp)}°</h2>`;
 
         card.addEventListener("click", ()=>{
             getWeather(city);
@@ -174,10 +217,13 @@ searchButton.addEventListener("click", ()=>{
 
 const toggleButton = document.querySelector("#toggleButton");
 const sideCard = document.querySelector("#sideCard");
+const background = document.getElementById("background");
 
-toggleButton.addEventListener("click", ()=>{
+toggleButton.addEventListener("click",()=>{
     sideCard.classList.toggle("collapsed");
+    background.classList.toggle("collapsed");
 })
+
 
 //storing new cities
 let cities =[];
